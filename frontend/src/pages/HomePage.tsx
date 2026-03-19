b46eae3f7b331389cced812234f7654ad3216d7b
NiksFenenko <niks.fenenko@gmail.com>
2026-03-19 17:26:20 +0200

    fix: change _id to id

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -76,7 +76,7 @@ function HomePage() {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {products.map((product) => (
-              <ProductCard key={product._id} product={product} />
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         )}
